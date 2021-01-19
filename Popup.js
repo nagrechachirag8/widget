4bc357fb7fd066e909227919e2f182b2f07f1b43
nagrechachirag8 <50979813+nagrechachirag8@users.noreply.github.com>
2021-01-19 13:38:12 +0530

    Add files via upload

--- Popup.js
+++ Popup.js
@@ -1,67 +1,36 @@
-var css = document.createElement('style')
-css.innerHTML =
-'body {font-family: Arial, Helvetica, sans-serif;}\
-.modal {\
-  display: none; \
-  position: fixed; \
-  z-index: 1; \
-  padding-top: 100px; \
-  left: 0;\
-  top: 0;\
-  width: 100%; \
-  height: 100%; \
-  overflow: auto; \
-  background-color: rgb(0,0,0); \
-  background-color: rgba(0,0,0,0.4); \
-}\
-.modal-content {\
-  background-color: #fefefe;\
-  margin: auto;\
-  padding: 20px;\
-  border: 1px solid #888;\
-  width: 80%;\
-}\
-.close {\
-  color: #aaaaaa;\
-  float: right;\
-  font-size: 28px;\
-  font-weight: bold;\
-}\
-.close:hover,\
-.close:focus {\
-  color: #000;\
-  text-decoration: none;\
-  cursor: pointer;\
-}'
+var link1 = document.createElement('link');
+link1.setAttribute('rel', 'stylesheet');
+link1.setAttribute('href', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
 
-document.body.innerHTML = 
-'<button id="myBtn">Open Modal</button>\
-<div id="myModal" class="modal">\
+var script1 = document.createElement('script');
+script1.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js');
+
+var script2 = document.createElement('script');
+script2.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js');
+
+var script3 = document.createElement('script');
+script3.setAttribute('src', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js');
+
+
+var modelOpener = document.createElement('div');
+modelOpener.setAttribute('class', 'model fade');
+modelOpener.setAttribute('id', 'myModal');
+modelOpener.setAttribute('data-backdrop', 'false')
+
+modelOpener.innerHTML = '<div class="modal-dialog">\
 <div class="modal-content">\
-<span class="close">&times;</span>\
-Name: <input type="text" id="name"></br></br>\
-Email Id: <input type="text" id="name"></br></br>\
-<button type="button" id="okbtn">Ok</button>\
+  <div class="modal-header">\
+    <h4 class="modal-title">Modal Heading</h4>\
+    <button type="button" class="close" data-dismiss="modal">×</button>\
+  </div>\
+  <div class="modal-body">\
+    Name: <input type="text"></input>\
+  </div>\
+  <div class="modal-footer">\
+    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>\
+  </div>\
 </div>\
 </div>'
 
-document.head.appendChild(css);
-
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-var okbtn = document.getElementById("okbtn");
-okbtn.onclick = function(){
-  modal.style.display = "none";
-}
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-span.onclick = function() {
-  modal.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+document.head.append(link1, script1, script2, script3)
+document.body.appendChild(modelOpener);
